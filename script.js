12695b746bc9d0756518c4003b7128bcfb47244f
Koyun92 <78324188+Koyun92@users.noreply.github.com>
2022-12-08 10:37:09 +0100

    Adding script
    Script to operate scroll navigation

---
+++ script.js
@@ -0,0 +1,21 @@
+<script>
+const menuItems = document.querySelectorAll('.menu-3 li');
+
+
+menuItems.forEach((item)=>{
+	item.addEventListener('click',(e)=>{
+		const fixedNav = document.querySelector('.headerWrapper');
+		console.log(getComputedStyle(fixedNav, null).position);
+		const scrollItem = document.querySelector(`#${e.target.dataset.name}`)
+		window.scroll({
+			top:`${scrollItem.getBoundingClientRect().top + window.pageYOffset - (getComputedStyle(fixedNav, null).position == "fixed" ? fixedNav.offsetHeight : 0 )}`,
+			left:0,
+			behavior:'smooth'})
+		
+		console.log(scrollItem.offsetTop + window.pageYOffset);
+	})
+})
+
+
+
+</script>
